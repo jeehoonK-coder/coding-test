6ca7d56a2fc8495ee4b6fbc693da506ee6c9ebf7
Kang Jee Hoon <shb04016@gmail.com>
2022-03-17 19:01:41 +0900

    Level1 Question

---
+++ src/Level1_2.java
@@ -0,0 +1,32 @@
+//Level1
+//폰켓몬
+
+import java.util.*;
+
+public class Level1_2 {
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,3};
+        int num = nums.length/2;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int number:nums){
+            set.add(number);
+        }
+        System.out.println(num>set.size() ? set.size() : num);
+
+    }
+}
+
+/*
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int num = nums.length/2;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int number:nums){
+            set.add(number);
+        }
+        return num>set.size() ? set.size() : num;
+    }
+}
+ */
