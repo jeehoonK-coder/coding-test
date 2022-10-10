1617cdfbc08bf47f923dcdbfe9f6baee02f7f144
Kang Jee Hoon <shb04016@gmail.com>
2022-10-10 17:43:41 +0900

    Back_No_11659
    - 구간 합 구하기4
    - DP활용

---
+++ src/Algorithm/DynamicProgramming/Back_11659.java
@@ -0,0 +1,33 @@
+//Back_11659 구간 합 구하기4 (실버)
+//DP
+
+package Algorithm.DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Back_11659 {
+    public void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        if(n == 1) dp[1] = 1;
+        else if(n == 2) dp[2] = 2;
+        else if(n == 3) dp[3] = 3;
+        else{
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 3;
+            for(int i = 4; i <= n; i++){
+                dp[i] = (dp[i-1]+dp[i-2])%10007;
+            }
+        }
+        System.out.println(dp[n]);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new Back_11659().solution();
+    }
+}
