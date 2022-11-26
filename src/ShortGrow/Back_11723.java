bdcd4e0ae558bb72cc63854e12d567754ecf0aab
Kang Jee Hoon <shb04016@gmail.com>
2022-11-26 16:56:08 +0900

    Back_11723
    - 집합
    - 비트마스킹

---
+++ src/ShortGrow/Back_11723.java
@@ -0,0 +1,74 @@
+//Back_11723 집합 (실버)
+//집합, 비트마스킹..?
+
+package ShortGrow;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Back_11723 {
+
+    static int M;
+    static String Str;
+    static Set<Integer> set = new HashSet<>();
+
+
+    public void solution() throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        M = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            Str = st.nextToken();
+            switch(Str){
+                case "add":
+                    set.add(Integer.parseInt(st.nextToken()));
+                    break;
+
+                case "remove":
+                    set.remove(Integer.parseInt(st.nextToken()));
+                    break;
+
+                case "check":
+                    if(set.contains(Integer.parseInt(st.nextToken()))) bw.write(1+"\n");
+                    else{
+                        bw.write(0+"\n");
+                    }
+                    break;
+
+                case "toggle":
+                     int k = Integer.parseInt(st.nextToken());
+                     if(set.contains(k)) set.remove(k);
+                     else{
+                         set.add(k);
+                     }
+                     break;
+
+                case "all":
+                    set = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+                    break;
+
+                case "empty":
+                    set = new HashSet<>();
+                    break;
+            }
+        }
+
+        bw.flush();
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new Back_11723().solution();
+    }
+}
