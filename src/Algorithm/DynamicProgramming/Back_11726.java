b915089c16fbab7254b506a996ea5344561ec881
Kang Jee Hoon <shb04016@gmail.com>
2022-12-13 23:58:49 +0900

    Back_No_11726 (fix)
    - 2xn 타일링
    - DP

--- src/Algorithm/DynamicProgramming/Back_11726.java
+++ src/Algorithm/DynamicProgramming/Back_11726.java
@@ -1,29 +1,36 @@
 //Back_11726 2xn 타일링 (실버)
-//DP
+//DP를 사용하지 않으면 시간초과
 
 package Algorithm.DynamicProgramming;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Back_11726 {
     public void solution() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
-        if(n == 1) dp[1] = 1;
-        else if(n == 2) dp[2] = 2;
-        else if(n == 3) dp[3] = 3;
-        else{
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 3;
-            for(int i = 4; i <= n; i++){
-                dp[i] = (dp[i-1]+dp[i-2])%10007;
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            num[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(dp[n]);
+        int[] dp = new int[n+1];
+        for(int i = 1; i<=n ; i++){
+            dp[i] = dp[i-1]+num[i-1];
+        }
+        for(int i = 0; i < k ;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(dp[b]-dp[a-1]).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
     }
 
 
