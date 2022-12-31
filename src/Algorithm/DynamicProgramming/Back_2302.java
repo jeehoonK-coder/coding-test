aa3229311b36689239d4c68cf2fd7f203f7764b3
Kang Jee Hoon <shb04016@gmail.com>
2023-01-01 00:04:07 +0900

    Back_No_2302(fix)
    - 극장 좌석
    - DP

--- src/Algorithm/DynamicProgramming/Back_2302.java
+++ src/Algorithm/DynamicProgramming/Back_2302.java
@@ -19,30 +19,38 @@ public class Back_2302 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        DP = new int[N+1];
-        for(int i = 0; i < M; i++){
-            Array[i] = Integer.parseInt(br.readLine());
+        if(N == 1) {
+            bw.write(1+"");
+            bw.flush();
+            bw.close();
+            br.close();
+            return;
         }
+        DP = new int[N+1];
         DP[0] = 1;
         DP[1] = 1;
         DP[2] = 2;
-        Array = new int[M];
         for(int i = 3; i <= N; i++){
             DP[i] = DP[i-1]+DP[i-2];
         }
         if(M == 0) {
             bw.write(DP[N] + "");
+            bw.flush();
+            bw.close();
+            br.close();
+            return;
         }
-        else if(N == 1) bw.write(1+"");
-        else{
-            int answer = 1;
-            answer *= DP[Array[0] -1];
-            for(int i = 0; i < M-1; i++){
-                answer *= DP[(Array[i+1] - Array[i] - 1)];
-            }
-            answer *= DP[N-Array[M-1]];
-            bw.write(answer+"");
+        Array = new int[M];
+        for(int i = 0; i < M; i++){
+            Array[i] = Integer.parseInt(br.readLine());
         }
+        int answer = 1;
+        answer *= DP[Array[0] -1];
+        for(int i = 0; i < M-1; i++){
+            answer *= DP[(Array[i+1] - Array[i] - 1)];
+        }
+        answer *= DP[N-Array[M-1]];
+        bw.write(answer+"");
         bw.flush();
         bw.close();
         br.close();
