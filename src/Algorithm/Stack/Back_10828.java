18acda2209eb8bea9ebd308d505909c0986c5b57
Kang Jee Hoon <shb04016@gmail.com>
2023-01-16 14:03:04 +0900

    Back_No_10828(fix)
    - 스택
    - 스택

--- src/Algorithm/Stack/Back_10828.java
+++ src/Algorithm/Stack/Back_10828.java
@@ -1,36 +1,84 @@
-package Algorithm.Stack;
+//Back_10828 스택 (실버)
+//스택
 
-import ShortGrow.Back_15683;
+package Algorithm.Stack;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Back_10828 {
-    static int N,M;
-    static char[][] Array;
+    static int N;
+    static Stack<Integer> Stack = new Stack<>();
 
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
         for(int i = 0; i < N; i++){
-            Array[i] = br.readLine().toCharArray();
+            st = new StringTokenizer(br.readLine());
+            if(st.countTokens() == 1) {
+                String str = st.nextToken();
+                switch(str){
+                    case "pop":
+                        bw.write(pop()+"\n");
+                        break;
+                    case "size":
+                        bw.write(stackSize()+"\n");
+                        break;
+                    case "empty":
+                        bw.write(empty()+"\n");
+                        break;
+                    case "top":
+                        bw.write(top()+"\n");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else{
+                st.nextToken();
+                push(Integer.parseInt(st.nextToken()));
+            }
         }
-
 
         bw.flush();
         bw.close();
         br.close();
     }
 
+    public void push(int x) {
+        Stack.push(x);
+    }
+
+    public int pop() {
+        if(Stack.isEmpty()) return -1;
+        return Stack.pop();
+    }
+
+    public int stackSize(){
+        return Stack.size();
+    }
+
+    public int empty(){
+        if(Stack.isEmpty()) return 1;
+        else return 0;
+    }
+
+    public int top(){
+        if(Stack.isEmpty()) return -1;
+        return Stack.peek();
+    }
+
+
     public static void main(String[] args) throws Exception {
         new Back_10828().solution();
     }
-}
+
 }
