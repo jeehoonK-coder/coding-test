9b1ed6800f824fdd85943b53a102aace23fd2426
Kang Jee Hoon <shb04016@gmail.com>
2023-01-28 16:28:17 +0900

    Back_No_1477 (fix)
    - 휴게소 세우기
    - 이분 탐색

--- src/algorithm/binarysearch/Back_1477.java
+++ src/algorithm/binarysearch/Back_1477.java
@@ -1,5 +1,7 @@
 //Back_1477 휴게소 세우기
 //이분 탐색
+//Shelter배열에 0과 L을 추가로 넣어서 구현하면 N이 0일때와 1일때의 조건을 넣을 필요가 없다!!
+
 
 package algorithm.binarysearch;
 
@@ -11,49 +13,70 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Back_1477 {
-    /*
-    static int M,N;
-    static long[] SnackList;
+
+    static int N,M,L;
+    static int[] Shelter;
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        SnackList = new long[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            SnackList[i] = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        if(N==0){
+            bw.write((int)Math.ceil(L / (double)(M+1)) +"");
         }
-        Arrays.sort(SnackList);
-        long max = SnackList[N-1];
-        bw.write(binarySearch(max) + "" );
+        else{
+            Shelter = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++){
+                Shelter[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(Shelter);
+            bw.write(binarySearch(L) + "" );
+        }
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public long binarySearch(long max) {
-        long begin = 1, end = max;
-        long mid = 0, pivot = 0;
-        long result = 0;
+
+    public int binarySearch(int max) {
+        int begin = 1, end = max;
+        int mid = 0, pivot = 0;
+        int result = 0;
         while(begin <= end){
             pivot = (begin+end) >> 1;
             mid = 0;
-            for(long i : SnackList){
-                mid += i / pivot;
+            if(N == 1){
+                mid += Math.ceil((L-Shelter[0]) / (double)pivot) - 1;
+                mid += Math.ceil((Shelter[0]) / (double)pivot) - 1;
             }
-            if(mid >= M) {
-                result = pivot;
+            else{
+                for(int i = 0; i < N; i++){
+                    if(i == 0) {
+                        mid += Math.ceil(Shelter[i] / (double)pivot) - 1;
+                    }
+                    else if(i == N-1) {
+                        mid += Math.ceil((L-Shelter[i]) / (double)pivot) - 1;
+                        mid += Math.ceil((Shelter[i] - Shelter[i-1]) / (double)pivot) - 1;
+                    }
+                    else mid += Math.ceil((Shelter[i] - Shelter[i-1]) / (double)pivot) - 1;
+                }
+            }
+            if(mid > M) {
                 begin = pivot+1;
             }
-            else end = pivot -1;
+            else {
+                result = pivot;
+                end = pivot -1;
+            }
         }
 
         return result;
     }
-    */
+
     public static void main(String[] args) throws Exception{
         new Back_1477().solution();
     }
