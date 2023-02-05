ebde63f732fa108198bf6aaa41880a215dda815e
Kang Jee Hoon <shb04016@gmail.com>
2023-02-05 16:11:35 +0900

    Back_No_3151 (fix)
    - 합이 0
    - 투 포인터, 이분탐색

--- src/algorithm/binarysearch/Back_3151.java
+++ src/algorithm/binarysearch/Back_3151.java
@@ -1,54 +1,76 @@
-package algorithm.binarysearch;
+//Back_3151 합이 0
+//투 포인터, 이분탐색
 
-import algorithm.twopoint.Back_2467;
+package algorithm.binarysearch;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Back_3151 {
 
     static int N;
-    static int[] LiquidList;
+    static int[] CodingList;
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        LiquidList = new int[N];
+        CodingList = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
-            LiquidList[i] = Integer.parseInt(st.nextToken());
+            CodingList[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(CodingList);
 
-        bw.write(twoPointSearch());
+        bw.write(Search() + "");
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public String twoPointSearch(){
-        int begin = 0, end = N-1;
-        int mid = LiquidList[0] + LiquidList[N-1];
-        int left = LiquidList[0], right = LiquidList[N-1];
-        while(begin < end){
-            if( Math.abs(LiquidList[begin] + LiquidList[end]) < Math.abs(mid) ){
-                mid = LiquidList[begin] + LiquidList[end];
-                left = LiquidList[begin];
-                right = LiquidList[end];
-            }
+    public long Search(){
+        long count = 0L;
+        for(int i = 0; i < N-2; i++){
+            if(CodingList[i] > 0) break;
+            int begin = i+1, end = N-1;
+            int mid = 0;
 
-            if( Math.abs(LiquidList[end]) > Math.abs(LiquidList[begin])){
-                end = end-1;
+            while(begin < end){
+                int s = 1;
+                int e = 1;
+                mid = CodingList[begin] + CodingList[end];
+                if(mid + CodingList[i] == 0 && CodingList[begin] == CodingList[end]){
+                    int a = end - begin +1;
+                    count += (a*(a-1)) / 2;
+                    break;
+                }
+
+                if(mid + CodingList[i] == 0){
+                    while (begin + 1 < end && CodingList[begin] == CodingList[begin + 1]) {
+                        s++;
+                        begin++;
+                    }
+                    while (begin < end - 1 && CodingList[end] == CodingList[end - 1]) {
+                        e++;
+                        end--;
+                    }
+                    count += s * e;
+                }
+                if(mid + CodingList[i] > 0){
+                    end -= 1;
+                }
+                else{
+                    begin +=1;
+                }
             }
-            else begin = begin+1;
         }
 
-        return left+" "+right;
+        return count;
     }
-
 
     public static void main(String[] args) throws Exception {
         new Back_3151().solution();
