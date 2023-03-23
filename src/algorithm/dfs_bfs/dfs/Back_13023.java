d89ed85c4fc3dc0c1eea0a556e51c923e82542eb
JiHun <shb04016@gmail.com>
2023-03-23 19:24:00 +0900

    Back_No_13023
    - ABCDE
    - DFS

---
+++ src/algorithm/dfs_bfs/dfs/Back_13023.java
@@ -0,0 +1,73 @@
+/**
+ *  23.03.23
+ *  Back_13023 ABCDE
+ *  DFS
+ */
+
+package algorithm.dfs_bfs.dfs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Back_13023 {
+
+    static int N,M,Answer = 0;
+    static List<Integer>[] RelationList;
+
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        RelationList = new List[N];
+        for(int i = 0; i < N; i++){
+            RelationList[i] = new ArrayList();
+        }
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            RelationList[a].add(b);
+            RelationList[b].add(a);
+        }
+        int[] check;
+        for(int i = 0; i < N; i++){
+            if(Answer == 1){
+                break;
+            }
+            check = new int[N];
+            DFS(i, 0,check);
+        }
+        bw.write(Answer+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public void DFS(int num, int depth, int[] check){
+        if(depth == 3){
+            Answer = 1;
+        }
+        check[num] = 1;
+        for(int n : RelationList[num]){
+            if(check[n] == 0){
+                DFS(n, depth+1, check);
+                check[n] = 0;
+            }
+        }
+
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new Back_13023().solution();
+    }
+}
